e46ee57c6196b430ea9c1eb7d5036f4a9ca1df5d
Luiz Henrique Almeida da Silva <luizhrqas@gmail.com>
2018-02-21 11:11:41 -0300

    init new route

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import logoJpg from "./assets/logo.jpg";
 import AppRouter from "./routes";
 import "./stylesheet.css";
 
@@ -6,6 +8,20 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        <Grid>
+          <Row center="xs">
+            <Col xs={12}>
+              <img
+                src={logoJpg}
+                style={{
+                  width: "128px ",
+                  marginTop: "2em",
+                  marginBottom: "2em"
+                }}
+              />
+            </Col>
+          </Row>
+        </Grid>
         <AppRouter />
       </div>
     );
--- client/src/app/routes/Home.js
+++ client/src/app/routes/Home.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
-import logoJpg from "../assets/logo.jpg";
 import Card, { CardActions, CardContent } from "material-ui/Card";
 import Button from "material-ui/Button";
 import Typography from "material-ui/Typography";
 import Icon from "material-ui/Icon";
+import { withRouter } from "react-router-dom";
 
-const Home = () => (
-  <Grid>
-    <Row center="xs">
-      <Col xs={12}>
-        <img
-          src={logoJpg}
-          style={{ width: "128px ", marginTop: "2em", marginBottom: "2em" }}
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} md={3}>
-        <Card>
-          <CardContent>
-            <Typography variant="headline" component="h2">
-              Vendas
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button color="primary">
-              <Icon>add</Icon> Criar Nova
-            </Button>
-            <Button color="primary">
-              <Icon>list</Icon> Listar
-            </Button>
-          </CardActions>
-        </Card>
-      </Col>
-    </Row>
-  </Grid>
-);
+const Home = ({ history }) => {
+  const redirectTo = route => {
+    history.push(route);
+  };
 
-export default Home;
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12} md={3}>
+          <Card>
+            <CardContent>
+              <Typography variant="headline" component="h2">
+                Vendas
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button color="primary" onClick={() => redirectTo("/sales/new")}>
+                <Icon>add</Icon> Criar Nova
+              </Button>
+              <Button color="primary" onClick={() => redirectTo("/sales/all")}>
+                <Icon>list</Icon> Listar
+              </Button>
+            </CardActions>
+          </Card>
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
+
+export default withRouter(Home);
